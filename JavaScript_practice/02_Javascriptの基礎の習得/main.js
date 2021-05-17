45abd2c8d9aad2c07eec74efdfe954a0c0624e81
macchino <mac.kimitoshi@gmail.com>
2021-05-17 15:34:10 +0900

    piriod

--- JavaScript_practice/02_Javascriptの基礎の習得/main.js
+++ JavaScript_practice/02_Javascriptの基礎の習得/main.js
@@ -1,2 +1,48 @@
 'use strict';
 console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+console.log('Hello World');
+
+console.log(12345);
+console.log(12345.6789);
+console.log('12345');
+console.log("hello! I'm juda");
+console.log("hello! I\'m juda");
+console.log("hel\ \tlo! I\'m \njuda");
+
+let color = 'Red';
+console.log(color);
+
+let myColor = 'Blue';
+console.log(myColor);
+
+var myName = 'Taro';
+console.log(myName);
+
+// 定数
+const PI = 3.14;
+console.log(PI);
+// PI = 3.1; NG
+
+const TAX_RATE = 10;
+console.log(TAX_RATE);
+
+let MyName = 'Taro';
+let num = 123;
+let dec = 123.456;
+
+console.log(MyName);
+console.log(num);
+console.log(dec);
+
+console.log(typeof MyName);
+console.log(typeof num);
+console.log(typeof dec);
+変数に型違いが編入出来てしまう
+MyName = 456;
+console.log(MyName);
+console.log(typeof MyName);
+
+// JavaScript=動的型付け言語
+
